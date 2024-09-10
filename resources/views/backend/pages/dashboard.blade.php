2f81c0c218855b3b7e0421331c3b25ef274a3fef
shalahuddin <mdshalahuddin004@gmail.com>
2024-09-11 03:27:14 +0600

    Ecommerce backend login dashoboard

--- resources/views/backend/pages/dashboard.blade.php
+++ resources/views/backend/pages/dashboard.blade.php
@@ -1,6 +1,26 @@
 @extends('backend.layouts.master')
-@section('title', 'dashboard page')
+
+@section('title')
+    Dashboard
+@endsection
+
+@push('admin_style')
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css">
+
+    <style>
+        .dataTables_length {
+            padding: 20px 0;
+        }
+    </style>
+@endpush
 
 @section('admin_content')
-    <h1>dashboard</h1>
+    <div class="row">
+        <div class="col-12">
+            <h1>Welcome to Dashboard</h1>
+        </div>
+    </div>
 @endsection
+
+@push('admin_script')
+@endpush
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\backend\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +15,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/dashboard', function () {
-    return view('backend.pages.dashboard');
-});
+
 Route::get('/', function () {
     return view('frontend.pages.home');
 });
+
+/*Admin Auth routes */
+Route::prefix('admin/')->group(function(){
+    Route::get('login', [LoginController::class, 'loginPage'])->name('admin.loginpage');
+    Route::post('login', [LoginController::class, 'login'])->name('admin.login');
+    Route::get('logout', [LoginController::class, 'logout'])->name('admin.logout');
+
+
+
+    Route::middleware(['auth'])->group(function(){
+        Route::get('dashboard', [DashboardController::class, 'dashboard'])->name('admin.dashboard');
+
+
+
+    });
+
+
+
+});
+/*Admin Auth routes */
